e4f324685fb0d78888bf873c015218b5bc6ec85e
unsafePtr <unsafeptr@gmail.com>
2017-06-11 18:36:30 +0300

    - add template and AppHarborDbContext

---
+++ AppHarbor/DbAccess/AppHarborDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAccess
+{
+    public class AppHarborDbContext : DbContext, IAppHarborDbContext
+    {
+        public AppHarborDbContext(string connectionString) : base(connectionString)
+        {
+
+        }
+    }
+}
